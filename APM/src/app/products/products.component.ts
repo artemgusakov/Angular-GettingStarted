5c13316ea403d871b8ee5a6dbf541106c317bed8
artem <artyom.gusakov@gmail.com>
2019-05-03 01:34:18 +0300

    Services and Dependency Injection

--- APM/src/app/products/products.component.ts
+++ APM/src/app/products/products.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Product} from './product';
+import {ProductService} from './product.service';
 
 @Component({
   selector: 'pm-products',
@@ -13,37 +14,7 @@ export class ProductsComponent implements OnInit {
   imgMargin = 2;
   showImage = false;
   filteredProducts: Product[];
-  products: Product[] = [
-    {
-      'productId': 1,
-      'productName': 'Leaf Rake',
-      'productCode': 'GDN-0011',
-      'releaseDate': 'March 19, 2016',
-      'description': 'Leaf rake with 48-inch wooden handle.',
-      'price': 19.95,
-      'starRating': 3.2,
-      'imageUrl': 'https://image.flaticon.com/icons/svg/1653/1653783.svg'
-    },
-    {
-      'productId': 2,
-      'productName': 'Garden Cart',
-      'productCode': 'GDN-0023',
-      'releaseDate': 'March 18, 2016',
-      'description': '15 gallon capacity rolling garden cart',
-      'price': 32.99,
-      'starRating': 4.2,
-      'imageUrl': 'https://image.flaticon.com/icons/svg/1653/1653782.svg'
-    },
-    {
-      'productId': 5,
-      'productName': 'Hammer',
-      'productCode': 'TBX-0048',
-      'releaseDate': 'May 21, 2016',
-      'description': 'Curved claw steel hammer',
-      'price': 8.9,
-      'starRating': 4.8,
-      'imageUrl': 'https://image.flaticon.com/icons/svg/1653/1653798.svg'
-    }];
+  products: Product[];
 
   private _filter: string;
 
@@ -57,13 +28,12 @@ export class ProductsComponent implements OnInit {
     this.filteredProducts = this.filter ? this.getFilteredProducts(this.filter) : this.products;
   }
 
-  constructor() {
-    this.filteredProducts = this.products;
-    this.filter = 'cart';
-  }
+  constructor(private _productService: ProductService) {}
 
   ngOnInit() {
-  }
+    this.products = this._productService.getProducts();
+    this.filteredProducts = this.products;
+ }
 
   toggleImage(): void {
     this.showImage = !this.showImage;
